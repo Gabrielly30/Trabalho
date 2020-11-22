ed62d13736446da4454f743266232f74e08757a8
Gabrielly30 <72361638+Gabrielly30@users.noreply.github.com>
2020-11-22 20:38:47 -0300

    APS
    APS

---
+++ services/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAH95jnRRTjvVHJTT3UVcl1rr9XAoUypGk",
+    authDomain: "grupou10.firebaseapp.com",
+    databaseURL: "https://grupou10.firebaseio.com",
+    projectId: "grupou10",
+    storageBucket: "grupou10.appspot.com",
+    messagingSenderId: "550484340851",
+    appId: "1:550484340851:web:4606555aa1f17895c23a06"
+  };
+
+  firebase.initializeApp(firebaseConfig);
